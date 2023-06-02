bfc75653b8398ca2c5868d1adc18933de6d217ce
Caeleb <khocaeleb@gmail.com>
2023-06-01 18:29:35 -0700

    card animation for delete

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation';
+import { useSpring, animated } from "@react-spring/web"
 
 interface CardProps {
     id: string,
@@ -13,21 +14,30 @@ export default function Card(props: CardProps) {
     const { id, term, definition, editMode, deleteCard } = props
     const router = useRouter()
 
+    const buttonGrow = useSpring({
+        from: {
+            opacity: 0,
+            scale: 0,
+            maxWidth: "0px",
+        },
+        to: {
+            opacity: editMode ? 1 : 0,
+            scale: editMode ? 1 : 0,
+            maxWidth: editMode ? "100%" : "0px",
+        },
+    })
+
     return (
-        <li className="flex items-center m-2">
-            {editMode ?
-                <button onClick={() => {
-                    deleteCard(id)
-                    router.refresh()
-                }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 p-0.5
-                stroke-red-500 border border-red-500 rounded-full mr-2 hover:cursor-pointer hover:bg-slate-600">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                :
-                <></>
-            }
+        <li className="flex gap-2 items-center m-2">
+            <animated.button style={buttonGrow} onClick={() => {
+                deleteCard(id)
+                router.refresh()
+            }}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 p-0.5
+            stroke-red-500 border border-red-500 rounded-full hover:cursor-pointer hover:bg-slate-600">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </animated.button>
             <div className="flex grow justify-between items-center border border-slate-300 text-slate-300 rounded px-2 py-1">
                 <span>{term}</span>
                 <span>{definition}</span>
